375a1034da02d6fabdaf0fcd4360b8d74c7d12a4
stilt0n <mattias.leino@arts.cornish.edu>
2024-01-10 22:26:09 -0500

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+const LANGUAGE_SERVER_URL = "http://localhost:5150/eval";
+
+const button = document.getElementById("code-submission");
+button.addEventListener("click", async () => {
+  const codeEditor = document.getElementById("code-editor");
+  const evalOutput = document.getElementById("eval-output");
+  const response = await fetch(LANGUAGE_SERVER_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code: codeEditor.value,
+    }),
+  });
+  const json = await response.json();
+  console.log(json);
+  const output = json.result;
+  evalOutput.innerText = output;
+});

ad8d617a14f06f21c8e7cbd173975392a5f4e8bf
stilt0n <mattias.leino@arts.cornish.edu>
2024-01-13 19:22:36 -0500

    add more comprehensive example code

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ button.addEventListener("click", async () => {
   evalOutput.innerText = output;
   evalOutput.className = json.isError ? "red" : "yellow";
 });
+
+const exampleCode = `let capitalized = fn(word) {
+  let chars = split(word, "");
+  let firstChar = toUpperCase(first(chars));
+  let restChars = toLowerCase(join(rest(chars), ""));
+  return firstChar + restChars;
+}
+
+let words = split("welcome TO THE " + -"yeknOm" + " Programming Language", " ");
+let output = []
+let i = 0;
+while (i < len(words)) {
+  let output = push(output, capitalized(words[i]));
+  let i = i + 1;
+};
+join(output, " ") + "!"
+`;
+
+const codeEditor = document.getElementById("code-editor");
+codeEditor.value = exampleCode;
